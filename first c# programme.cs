02ac34848708e2553776a4b0446263a26871baa1
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-03-30 00:13:34 +0530

    first c# programme.cs
    first c# programme using c#.
    
    Coding blueprint Hello World!

---
+++ first c# programme.cs
@@ -0,0 +1,17 @@
+//oshe ravanz contribution
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oshe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
