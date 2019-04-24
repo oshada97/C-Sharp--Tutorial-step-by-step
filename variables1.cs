8989450e7c27680fd017527c274aa88bf63ad5ce
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-24 23:22:31 +0530

    Update variables1.cs

--- variables1.cs
+++ variables1.cs
@@ -1,3 +1,3 @@
-int myAge = 21; //int is data type  , in this statement actually done by declarinf variable
+int myAge = 21; //int is data type  , in this statement actually done by declare ""int"" variable
 
 myAge = 21;  //we can say assigning variable
