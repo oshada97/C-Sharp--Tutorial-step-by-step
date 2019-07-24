14c91aa0018f4f07be1a554bd0257dc7489c4cd2
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-07-24 20:10:21 +0530

    function t1
    function t1

---
+++ function without return.cs
@@ -0,0 +1,25 @@
+//oshada97
+//oshe ravanz contribution
+//enjoy open source
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oshada97
+{
+    class Program
+    {
+        static void sayHey()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Codes");
+        }
+        static void Main(string[] args)
+        {
+            sayHey();
+        }
+    }
+}
